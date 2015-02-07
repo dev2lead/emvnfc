390e3edc40151696938f6271e308a2d038ee7f53
Nicolas THIBAUT <nicolas@dev2lead.com>
2015-02-07 18:22:44 +0100

    Patch for Android Lollipop

--- app/src/main/java/com/dev2lead/emvnfc/MainActivity.java
+++ app/src/main/java/com/dev2lead/emvnfc/MainActivity.java
@@ -144,7 +144,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -154,9 +154,12 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
